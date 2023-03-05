058edbb146d66a1c5b45c243d3fa4a1f25ec68f8
Ali Karaoglan <karaoglan.a.m@gmail.com>
2023-03-05 21:58:19 +0300

    CI-CD

--- src/test/java/cbt/Driver.java
+++ src/test/java/cbt/Driver.java
@@ -35,7 +35,7 @@ public class Driver {
                 case "remote-chrome":
                     try {
                         // assign your grid server address
-                        String gridAddress = "107.23.174.248";
+                        String gridAddress = "54.198.85.221";
                         URL url = new URL("http://" + gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("chrome");
